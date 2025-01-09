73c66513c02ff2f960901daf77cbb1844f6d670e
sas4211 <sas4211@gmail.com>
2025-01-09 14:46:50 +0500

    matcher update

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,6 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',],
 };
